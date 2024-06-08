c3e303e56fc4a1171e1c8377e700499d8a9094d4
Dwayne Robinson <dwayner@microsoft.com>
2024-06-07 20:34:10 -0700

    Article agreement in tools/lint.mjs comment

--- tools/lint.mjs
+++ tools/lint.mjs
@@ -225,7 +225,7 @@ for (const algorithm of root.querySelectorAll('.algorithm')) {
 
 // Prevent accidental normative references to other specs. This reports an error
 // if there is a normative reference to any spec *other* than these ones. This
-// helps avoid a autolink like [=object=] adding an unexpected reference to
+// helps avoid an autolink like [=object=] adding an unexpected reference to
 // [FILEAPI]. Add to this list if a new normative reference is intended.
 const NORMATIVE_REFERENCES = new Set([
   '[ECMASCRIPT]',
