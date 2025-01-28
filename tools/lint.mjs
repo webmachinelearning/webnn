1f72cca65b65ac4aa32515c3cf2cc54b1bb1ecac
Joshua Bell <jsbell@chromium.org>
2025-01-28 11:51:12 -0800

    Ensure object creation specifies the realm
    "Realm" is an ECMAScript concept best explained in
    https://html.spec.whatwg.org/multipage/webappapis.html#realms-and-their-counterparts
    Newly created JS objects must be associated with a Realm; while older
    specs didn't do this explicitly, best practice is to be explicit about
    this, especially for steps running "in parallel", or in algorithms
    separate from method steps. Do so!
    
    This also adds lint tests to try and catch future violations. Note
    that dictionaries (e.g. MLOperatorDescriptor) are Infra "ordered maps"
    it the body of spec algorithms, not JS objects, so they don't have a
    realm. Conversion to a JS object when returning a dictionary to script
    is handled by WebIDL bindings logic.
    
    Also note that DOMExceptions, either thrown or as promise rejection
    values, are not given a realm. This is a known issue across all web
    specs and is tracked in whatwg/webidl#135.
    
    Resolves #793.

--- tools/lint.mjs
+++ tools/lint.mjs
@@ -83,8 +83,9 @@ const root = parse(file, {
 });
 
 log('simplifying DOM...');
-// Remove script and style elements from consideration
-for (const element of root.querySelectorAll('script, style')) {
+// Remove script and style elements from consideration. Remove generated indexes
+// too, since they can lead to duplicate false-positive matches for lint rules.
+for (const element of root.querySelectorAll('script, style, .index')) {
   element.remove();
 }
 
@@ -338,6 +339,18 @@ for (const match of source.matchAll(/\|(\w+)\|\.{{(\w+)\/.*?}}/g)) {
       }
     });
   });
+}
+
+// Ensure JS objects are created with explicit realm
+for (const match of text.matchAll(/ a new promise\b(?! in realm)/g)) {
+  error(`Promise creation must specify realm: ${format(match)}`);
+}
+for (const match of text.matchAll(/ be a new ([A-Z]\w+)\b(?! in realm)/g)) {
+  const type = match[1];
+  // Dictionaries are just maps, so they don't need a realm.
+  if (type === 'MLOperandDescriptor')
+    continue;
+  error(`Object creation must specify realm: ${format(match)}`);
 }
 
 globalThis.process.exit(exitCode);
